0fda619ea597128e204d5ff926b525b51c9f0bb9
MeetaHaldar <meetahaldar1001@gmail.com>
2025-06-27 13:46:46 +0530

    Remove unused components and assets to streamline the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,5 @@
-import React from "react";
-import Navbar from "./Components/Navbar/Navbar";
-import Hero from "./Components/Hero/Hero";
-import Services from "./Components/Services/Services";
-import Title from "./Components/Title/Title";
-import About from "./Components/About/About";
-import Testimonials from "./Components/Testimonials/Testimonials";
-import Contact from "./Components/Contact/Contact";
-import Footer from "./Components/Footer/Footer";
 const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Hero />
-      <Title subtitle="Our Services" title="What we Offer" />
-      <Services />
-      <About />
-      <Title subtitle="TESTIMONIALS" title="What our experts says" />
-      <Testimonials />
-      <Title subtitle="Contact US" title="Get in Touch" />
-      <Contact />
-      <Footer />
-    </div>
-  );
+  return <div>hello</div>;
 };
 
 export default App;
